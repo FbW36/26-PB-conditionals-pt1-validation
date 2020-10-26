f81261b9b5b4b5ef7f696eb3263485b10afdad19
Zarzarius <zarzarius@gmail.com>
2020-10-26 22:18:08 +0100

    first commit

---
+++ Azael/index.js
@@ -0,0 +1,14 @@
+let userName = 'Alex'
+userName.length == 3 
+? 
+let userEmail = 'a.gmail.com'
+let userPassword = 'Alex22'
+let userConfirmPassword = 'alex22'
+```
+
+// * Check if the userName length bigger than 3 chr.
+// * Check if the userEmail length bigger than 12 chr and includes @.
+// * Check if the userPassword length bigger than 5 chr.
+// * Check if the userConfirmPassword equal to userPassword.
+
+
