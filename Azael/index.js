b45e11488af551ba3abfc11b84dddbc980e159bd
Zarzarius <zarzarius@gmail.com>
2020-10-26 22:30:40 +0100

    exercise done

--- Azael/index.js
+++ Azael/index.js
@@ -1,14 +1,26 @@
-let userName = 'Alex'
-userName.length == 3 
-? 
-let userEmail = 'a.gmail.com'
-let userPassword = 'Alex22'
-let userConfirmPassword = 'alex22'
-```
+// Check if the userName length bigger than 3 chr.
+let userName = "Alex";
+userName.length >= 3
+  ? console.log("User Name " + "The user data are valid")
+  : console.log("User Name " + "Validation error message");
+// Check if the userEmail length bigger than 12 chr and includes @.
 
-// * Check if the userName length bigger than 3 chr.
-// * Check if the userEmail length bigger than 12 chr and includes @.
-// * Check if the userPassword length bigger than 5 chr.
-// * Check if the userConfirmPassword equal to userPassword.
+let userEmail = "a.gmail.com";
+userEmail.length > 12 && userEmail.includes("@")
+  ? console.log("User Email " + "The user data are valid")
+  : console.log("User Email " + "Validation error message");
 
+// Check if the userPassword length bigger than 5 chr.
 
+let userPassword = "Alex22";
+userPassword.length > 5
+  ? console.log("User Password " + "The user data are valid")
+  : console.log("User Password " + "Validation error message");
+
+// Check if the userConfirmPassword equal to userPassword.
+
+let userConfirmPassword = "alex22";
+
+userConfirmPassword === userPassword
+  ? console.log("User Password Confirmation " + "The user data are valid")
+  : console.log("User Password Confirmation " + "Validation error message");
